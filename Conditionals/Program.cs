c92f4ce137ad20e75648d3446788ab990967b41e
lilykrichels <lily.krichels@gmail.com>
2017-08-21 20:50:17 -0400

    added conditional problems

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -21,23 +21,47 @@ namespace Conditionals
             //    Console.WriteLine("Welcome to the show!");
             //}
 
-            Console.WriteLine("What percentage did you recieve on this test?");
-            int grade = int.Parse(Console.ReadLine());
+            //Console.WriteLine("What percentage did you recieve on this test?");
+            //int grade = int.Parse(Console.ReadLine());
 
-            if (grade >= 90 && grade <= 100)
-            {
-                Console.WriteLine("You got an A.");
-            }
-            else if (grade >= 80 && grade <= 90)
-            {
-                Console.WriteLine("You got a B");
-            }
-            else if (grade >= 70 && grade <= 80)
-            {
-                Console.WriteLine("You have a C");
-            }
+            //if (grade >= 90 && grade <= 100)
+            //{
+            //    Console.WriteLine("You got an A.");
+            //}
+            //else if (grade >= 80 && grade <= 90)
+            //{
+            //    Console.WriteLine("You got a B");
+            //}
+            //else if (grade >= 70 && grade <= 80)
+            //{
+            //    Console.WriteLine("You have a C");
+            //}
 
+            //Console.WriteLine("Please enter a number.");
+            //int aNum = int.Parse(Console.ReadLine());
+            //if (aNum % 2 == 0)
+            //{
+            //    Console.WriteLine("That is an even number.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("That is an odd number.");
+            //}
 
+            //    Console.WriteLine("Please enter a number and I will tell you if it's positive or negative."); Console.WriteLine();
+            //    int newNum = int.Parse(Console.ReadLine());
+            //    if (newNum > 0)
+            //    {
+            //        Console.WriteLine("Your number is positive");
+            //    }
+            //    else if (newNum < 0)
+            //    {
+            //        Console.WriteLine("Your number is negative.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("That number is 0.");
+            //    }
         }
     }
 }
